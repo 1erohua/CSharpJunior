18a54a8987f5a0f3e6165a2eb459c5cbb70d2f49
零华 <3262064245@qq.com>
2023-06-20 21:20:12 +0800

    我草泥马

---
+++ C#匿名/Program.cs
@@ -0,0 +1,12 @@
+﻿//定义变量的时候可以不指定它的类型，而是使用var类型
+//一旦被赋值之后，匿名类型的变量将去掉隐匿，类型将一直确定，无法再被重新改写类型
+namespace C_匿名
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
